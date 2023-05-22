9fd19e81ff5dc26c620ebf3f5d5b1b9961727172
chrismdating1206@gmail.com <christian.dating12>
2023-05-22 21:58:12 +0800

    def input_num2 to user_interface file

--- user_interface.py
+++ user_interface.py
@@ -3,3 +3,7 @@ class user_interface:
     def ask_input1(self):
         num1 = float(input("Enter first number: "))
         return num1
+    
+    def ask_input2(self):
+        num2 = float(input("Enter second number: "))
+        return num2
