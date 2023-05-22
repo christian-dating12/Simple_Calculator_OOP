2725fe79be1d57e5a4fe5f4ccf91640e5db0e6ae
chrismdating1206@gmail.com <christian.dating12>
2023-05-22 21:01:16 +0800

    first commit

---
+++ program.py
@@ -0,0 +1,10 @@
+# Christian M. Dating | BSCPE 1-5 | MINI CALCULATOR
+
+# Create a Simple App Calculator | OOP
+# 1. The application will ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication, Division)
+# 2. The application will ask the user for two numbers.
+# 3. Display the result.
+# 4. The application will ask the user wants to try again or not.
+# 5. If yes, repeat step 1
+# 6. If no, Display "Thank you!" and the program will exit.
+# 7. Use Python Function and appropriate Exeptions to capture errors during runtime.
