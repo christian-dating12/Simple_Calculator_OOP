350b30ada4695df6800d78aa7085febc407dfd37
chrismdating1206@gmail.com <christian.dating12>
2023-05-22 22:18:00 +0800

    def Subtraction operation in calculator file

--- calculator.py
+++ calculator.py
@@ -3,3 +3,7 @@ class Calculator:
     def add(self, num1, num2):
         sum = num1 + num2
         return sum
+    
+    def subtract(self, num1, num2):
+        diff = num1 - num2
+        return diff
