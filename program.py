2a43d3ae3369c838570a634a60c6e2b42b966c65
chrismdating1206@gmail.com <christian.dating12>
2023-05-23 22:33:12 +0800

    if true, repeat step 1

--- program.py
+++ program.py
@@ -17,44 +17,59 @@ from calculator import Calculator
 ui = user_interface()
 calcu = Calculator()
 
-
+while True:
 # Ask the user for two inputs
 # input 1
-num1 = ui.ask_input1()
+    num1 = ui.ask_input1()
 # input 2
-num2 = ui.ask_input2()
+    num2 = ui.ask_input2()
 
 # Ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication, Division)
 # Math operations choices
-print("ADDITION")
-print("SUBTRACTION")
-print("MULTIPLICATION")
-print("DIVISION")
+    print("ADDITION")
+    print("SUBTRACTION")
+    print("MULTIPLICATION")
+    print("DIVISION")
 
-op = ui.input_op()
+
+    op = ui.input_op()
 
 # Display the result
-if op == ("ADDITION"):
-    sum = calcu.add(num1, num2)
-    ui.display_sum(sum)
+ 
+    try:
+        if op == ("ADDITION"):
+            sum = calcu.add(num1, num2)
+            ui.display_sum(sum)
 
-elif op == ("SUBTRACTION"):
-    diff = calcu.subtract(num1, num2)
-    ui.display_diff(diff)
+        elif op == ("SUBTRACTION"):
+            diff = calcu.subtract(num1, num2)
+            ui.display_diff(diff)
     
-elif op == ("MULTIPLICATION"):
-    prod = calcu.multiply(num1, num2)
-    ui.display_prod(prod)
+        elif op == ("MULTIPLICATION"):
+            prod = calcu.multiply(num1, num2)
+            ui.display_prod(prod)
 
-elif op == ("DIVISION"):
-    quot = calcu.divide(num1, num2)
-    ui.display_quot(quot)
+        elif op == ("DIVISION"):
+            quot = calcu.divide(num1, num2)
+            ui.display_quot(quot)
 
-else:
-    print("\nInvalid. Please choose from the given operations above.")
+        else:
+            print("\nInvalid. Please choose from the given operations above.")
 
-
-# Display the result
 # Ask the user to try again or not
+        while True:
+            again = ui.try_again()
+
 # If yes, repeat step 1
+
+            while True:
+                if again == "YES":
+                    break
+
+              
+                        
+    except ValueError:
+        while True:
+            print("Invalid input. Please try again.")
+            break
 # If no, display "Thank you"
